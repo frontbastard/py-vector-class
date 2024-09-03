c0b2b8965ebf5afae86b53e3b7bdff33231940ac
pavlo <frontbastard@gmail.com>
2024-09-03 14:15:50 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,55 @@
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x, y):
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        if isinstance(other, (int, float)):
+            return Vector(self.x * other, self.y * other)
+        elif isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y  # Return full precision result
+        else:
+            raise TypeError(
+                "Multiplication is only supported with numbers or Vectors"
+            )
+
+    @classmethod
+    def create_vector_by_two_points(cls, start_point, end_point):
+        return cls(
+            end_point[0] - start_point[0], end_point[1] - start_point[1]
+        )
+
+    def get_length(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def get_normalized(self):
+        length = self.get_length()
+        return Vector(self.x / length, self.y / length)
+
+    def angle_between(self, other):
+        dot_product = self * other
+        cos_angle = dot_product / (self.get_length() * other.get_length())
+        return round(math.degrees(math.acos(cos_angle)))
+
+    def get_angle(self):
+        angle = math.degrees(math.atan2(-self.x, self.y))
+        if angle < 0:
+            angle += 360
+        return round(angle)
+
+    def rotate(self, degrees):
+        radians = math.radians(degrees)
+        cos_angle = math.cos(radians)
+        sin_angle = math.sin(radians)
+        new_x = self.x * cos_angle - self.y * sin_angle
+        new_y = self.x * sin_angle + self.y * cos_angle
+        return Vector(new_x, new_y)
